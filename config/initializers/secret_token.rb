e7807ee24a7f61828150cf480c0384bcefd1ad76
foo <foo@mint>
2013-03-11 11:39:11 -0400

    rails new flailure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flailure::Application.config.secret_token = 'cb4a64806768a8f4e408ab939d84235fcd897b15b0c5d5bd523a1e17037d7e5a8256e0589556fdc41a99b6669e10d3e0ac993a979a17ce6208500f89e84d5e3c'
